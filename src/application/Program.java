26f91663f5a739bf934c12869499f4e2d90ed84f
Lucas Marques Mendes <lucasmqmendes@gmail.com>
2024-05-05 18:57:22 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,11 +31,13 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== TEST 3: seller insert ===");
-		Seller newSeller = new Seller(null, "Greg", "gref@gamil.com", new Date(), 4000.0, department);
-		sellerDao.insert(newSeller);
-		System.out.println("Inserted! new id = " + newSeller.getId());
-
+		
+		
+		System.out.println("\n=== TEST 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 	}
 
 }
